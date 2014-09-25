6ce12817446b1867b4c47fcb6824ddfc23a76118
wada811 <at.wada811@gmail.com>
2014-09-25 15:45:40 +0900

    Initial commit

---
+++ library/src/androidTest/java/at/wada811/android/material/design/colors/ApplicationTest.java
@@ -0,0 +1,13 @@
+package at.wada811.android.material.design.colors;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
