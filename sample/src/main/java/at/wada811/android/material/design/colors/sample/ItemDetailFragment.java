88958f99491e7df1c726a997d2dab212136a585a
wada811 <at.wada811@gmail.com>
2014-11-11 21:12:29 +0900

    Copyable color code on list click

--- sample/src/main/java/at/wada811/android/material/design/colors/sample/ItemDetailFragment.java
+++ sample/src/main/java/at/wada811/android/material/design/colors/sample/ItemDetailFragment.java
@@ -1,9 +1,11 @@
 package at.wada811.android.material.design.colors.sample;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ListFragment;
 import android.view.View;
+import android.widget.ListView;
 import java.util.List;
 import at.wada811.android.material.design.colors.sample.color.ColorPalette;
 import at.wada811.android.material.design.colors.sample.color.ColorPalette.ColorGroup;
@@ -39,4 +41,29 @@ public class ItemDetailFragment extends ListFragment{
         getListView().setDivider(null);
         getListView().setDividerHeight(0);
     }
+
+    @Override
+    public void onListItemClick(ListView listView, View view, int position, long id){
+        super.onListItemClick(listView, view, position, id);
+        MaterialDesignColor color = items.get(position);
+        copyText(getActivity(), getString(color.getColorCode()));
+    }
+
+    public void copyText(Context context, String text){
+        if(android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.HONEYCOMB){
+            android.text.ClipboardManager clipboard = (android.text.ClipboardManager)context.getSystemService(
+                Context.CLIPBOARD_SERVICE
+            );
+            clipboard.setText(text);
+        }else{
+            android.content.ClipboardManager clipboard = (android.content.ClipboardManager)context.getSystemService(
+                Context.CLIPBOARD_SERVICE
+            );
+            android.content.ClipData clip = android.content.ClipData.newPlainText(
+                "Copied Text", text
+            );
+            clipboard.setPrimaryClip(clip);
+        }
+    }
+
 }
